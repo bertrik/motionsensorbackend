0a7bb5f7b150e57b652686c625f647ce8bdf19c4
Bertrik Sikken <bertrik@sikken.nl>
2020-09-08 23:05:45 +0200

    Fix spotbugs errors.

--- motionsensorbackend/src/main/java/nl/bertriksikken/ttn/dto/TtnDownlinkMessage.java
+++ motionsensorbackend/src/main/java/nl/bertriksikken/ttn/dto/TtnDownlinkMessage.java
@@ -7,23 +7,23 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 public final class TtnDownlinkMessage {
 
     @JsonProperty("port")
-    int port;
+    private int port;
 
     @JsonProperty("confirmed")
-    boolean confirmed;
+    private boolean confirmed;
 
     @JsonProperty("payload_raw")
-    byte[] rawPayload;
+    private byte[] rawPayload = new byte[0];
 
     public TtnDownlinkMessage(int port, boolean confirmed, byte[] rawPayload) {
         this.port = port;
         this.confirmed = confirmed;
-        this.rawPayload = rawPayload;
+        this.rawPayload = rawPayload.clone();
     }
 
     @Override
     public String toString() {
-        return String.format(Locale.ROOT, "[port=%d,confirmed=%s]", port, confirmed);
+        return String.format(Locale.ROOT, "[port=%d,confirmed=%s,payload=%s]", port, confirmed, rawPayload);
     }
     
 }
