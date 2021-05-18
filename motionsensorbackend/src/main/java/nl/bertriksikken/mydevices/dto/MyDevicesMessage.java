47c1465bdd7015f8af210a180a9e7dd06defd75a
Bertrik Sikken <bertrik@sikken.nl>
2021-05-18 20:45:13 +0200

    Also send RSSI,SNR,SF to mydevices.

--- motionsensorbackend/src/main/java/nl/bertriksikken/mydevices/dto/MyDevicesMessage.java
+++ motionsensorbackend/src/main/java/nl/bertriksikken/mydevices/dto/MyDevicesMessage.java
@@ -2,6 +2,7 @@ package nl.bertriksikken.mydevices.dto;
 
 import java.util.ArrayList;
 
+import nl.bertriksikken.motionsensorbackend.BaseEvent;
 import nl.bertriksikken.motionsensorbackend.MotionEvent;
 import nl.bertriksikken.motionsensorbackend.TempHumidityEvent;
 
@@ -12,8 +13,16 @@ public final class MyDevicesMessage extends ArrayList<MyDevicesItem> {
 
     private static final long serialVersionUID = 1L;
 
-    public static MyDevicesMessage fromMotionEvent(MotionEvent event) {
+    private static MyDevicesMessage fromBaseEvent(BaseEvent event) {
         MyDevicesMessage message = new MyDevicesMessage();
+        message.add(new MyDevicesItem(100, "rssi", event.getRssi(), "dbm"));
+        message.add(new MyDevicesItem(101, "snr", event.getSnr(), "db"));
+        message.add(new MyDevicesItem(102, "analog_sensor", event.getSf(), "null"));
+        return message;
+    }
+    
+    public static MyDevicesMessage fromMotionEvent(MotionEvent event) {
+        MyDevicesMessage message = fromBaseEvent(event);
         message.add(new MyDevicesItem(1, "motion", (event.getOccupied() != 0) ? "1" : "0", "d"));
         message.add(new MyDevicesItem(2, "counter", Integer.toString(event.getCount()), "null"));
         message.add(new MyDevicesItem(3, "temp", event.getTemperature(), "c"));
@@ -23,7 +32,7 @@ public final class MyDevicesMessage extends ArrayList<MyDevicesItem> {
     }
 
     public static MyDevicesMessage fromHumidityEvent(TempHumidityEvent event) {
-        MyDevicesMessage message = new MyDevicesMessage();
+        MyDevicesMessage message = fromBaseEvent(event);
         message.add(new MyDevicesItem(2, "rel_hum", event.getHumidity(), "p"));
         message.add(new MyDevicesItem(3, "temp", event.getTemperature(), "c"));
         message.add(new MyDevicesItem(4, "voltage", event.getBattery(), "v"));
